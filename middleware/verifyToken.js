f47e19d616f58c7e38b32e1dacbd28507adbca6b
Soham Das <majumder70.ranu@gmail.com>
2022-01-07 14:30:28 +0530

    Admin functionalities, Delete User by Id, get User by ID, get all Users

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -27,4 +27,15 @@ const verifyTokenAndAuthorization = (req, res, next) => {
     })
 }
 
-module.exports = { verifyToken, verifyTokenAndAuthorization };
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if(req.user.isAdmin) {
+            next();
+        }
+        else {
+            res.status(403).json("you are not an admin!")
+        }
+    })
+}
+
+module.exports = { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin };
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { verifyToken, verifyTokenAndAuthorization } = require('../middleware/verifyToken');
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('../middleware/verifyToken');
 const User = require('../models/User');
 
 router.put("/:id", verifyTokenAndAuthorization, async(req, res) => {
@@ -11,6 +11,36 @@ router.put("/:id", verifyTokenAndAuthorization, async(req, res) => {
             $set: req.body,
         }, { new: true })
         res.status(200).json(updatedUser)
+    }catch(err) {
+        res.status(500).json(err);
+    }
+})
+
+// delete by admin only
+router.delete("/:id", verifyTokenAndAuthorization, async(req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User deleted!!");
+    }catch(err) {
+        res.status(500).json(err);
+    }
+})
+
+// get user by admin only
+router.get("/find/:id", verifyTokenAndAdmin, async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    }catch(err) {
+        res.status(500).json(err);
+    }
+})
+// get all the users
+router.get("/", verifyTokenAndAdmin, async(req, res) => {
+    try {
+        const users = await User.find()
+        res.status(200).json(users);
     }catch(err) {
         res.status(500).json(err);
     }
