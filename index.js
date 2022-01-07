d022478f9a7f79419870093375f0940982d2900e
Soham Das <majumder70.ranu@gmail.com>
2022-01-07 09:45:08 +0530

    MongoDB Connected!!

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Started server using Nodemon!")
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("MongoDB Connetcted Successfully!!"))
+    .catch((err) => console.log(err))
+
+app.listen(5000, () => {
+    console.log("SAS-REST-API server is up on running!!");
+})
