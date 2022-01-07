9ea2e32be2b7f55eeee619e67f3d1f9c3a5fd803
Soham Das <majumder70.ranu@gmail.com>
2022-01-07 20:20:14 +0530

    order route added

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require('dotenv');
 const userRoute = require('./routes/users') 
 const authRoute = require('./middleware/auth');
 const productRoute = require('./routes/products');
+const cartRoute = require('./routes/carts');
+const orderRoute = require('./routes/orders');
 
 dotenv.config();
 
@@ -17,6 +19,8 @@ app.use(express.json());
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/products", productRoute)
+app.use("/api/carts", cartRoute)
+app.use("/api/orders", orderRoute)
 app.listen(5000, () => {
     console.log("SAS-REST-API server is up on running!!");
 })
