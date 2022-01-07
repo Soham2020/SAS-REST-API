09f5f553170075b16dbbc19120d56001f356cdda
Soham Das <majumder70.ranu@gmail.com>
2022-01-07 11:18:04 +0530

    Models Added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoute = require('./routes/users') 
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ mongoose
     .then(() => console.log("MongoDB Connetcted Successfully!!"))
     .catch((err) => console.log(err))
 
+app.use(express.json());
+app.use("/api/users", userRoute)
 app.listen(5000, () => {
     console.log("SAS-REST-API server is up on running!!");
 })
---
+++ models/Product.js
@@ -0,0 +1,11 @@
+const mongoose= require('mongoose');
+
+const ProductSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true, unique: true },
+        price: { type: Number, required: true },
+        img: { type: String, required: true },
+    }, { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
