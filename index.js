e049c76d4f1c42e298baccbab856cb2af7bbae84
Soham Das <majumder70.ranu@gmail.com>
2022-01-08 09:51:14 +0530

    Changes in package.json

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const userRoute = require('./routes/users') 
 const authRoute = require('./middleware/auth');
 const productRoute = require('./routes/products');
 const cartRoute = require('./routes/carts');
 const orderRoute = require('./routes/orders');
+const stripeRoute = require('./routes/stripe');
 
 dotenv.config();
 
@@ -15,12 +17,17 @@ mongoose
     .then(() => console.log("MongoDB Connetcted Successfully!!"))
     .catch((err) => console.log(err))
 
+app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("<h1>SAS-REST-API is UP, and running!</h1>")
+})
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/products", productRoute)
 app.use("/api/carts", cartRoute)
 app.use("/api/orders", orderRoute)
+app.use("/api/stripe", stripeRoute)
 app.listen(5000, () => {
     console.log("SAS-REST-API server is up on running!!");
 })
