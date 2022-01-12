de82f57631e185834478792a53e374af24be0921
Soham Das <majumder70.ranu@gmail.com>
2022-01-12 20:15:47 +0530

    Post method -> Feedback Route

--- models/Feedback.js
+++ models/Feedback.js
@@ -3,8 +3,8 @@ const mongoose= require('mongoose');
 const FeedbackSchema = new mongoose.Schema(
     {
         username: { type: String, required: true},
-        Name: { type: String, required: true },
-        Message: { type: String, required: true },
+        name: { type: String, required: true },
+        message: { type: String, required: true },
     }, { timestamps: true }
 );
 
--- routes/feedback.js
+++ routes/feedback.js
@@ -1,4 +1,18 @@
 const router = require('express').Router();
 const Feedback = require('../models/Feedback');
 
+router.post("/post", async(req, res) => {
+    const newFeedback = new Feedback({
+        username: req.body.username,
+        name: req.body.name,
+        message: req.body.message,
+    });
+    try {
+        const saveFeedback = await newFeedback.save();
+        res.status(201).json(saveFeedback);
+    }catch(err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
